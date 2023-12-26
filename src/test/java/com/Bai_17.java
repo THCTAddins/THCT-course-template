1551a85b13bf7d2e787619aa631a3c356bb8fe4f
lulmaoatwork <lulmaoatwork@gmail.com>
2023-12-26 14:41:04 +0700

    Bai17

--- src/test/java/com/Bai_17.java
+++ src/test/java/com/Bai_17.java
@@ -13,7 +13,7 @@ public class Bai_17 extends BasicTest {
     @Test()
     public void loginTest() throws Exception {
         // Launch website
-        String url = "https://bantheme.xyz/hathanhauto/tai-khoan/aaaa";
+        String url = "https://bantheme.xyz/hathanhauto/tai-khoan/";
         driver.get(url);
         Assert.assertEquals(driver.getCurrentUrl(), url);
 
