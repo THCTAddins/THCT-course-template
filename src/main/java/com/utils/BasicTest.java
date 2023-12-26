f308fe845691337ee5062e47c559af04ca04f23e
lulmaoatwork <lul@gmail.com>
2023-12-26 14:16:51 +0700

    Bai 17

--- src/main/java/com/utils/BasicTest.java
+++ src/main/java/com/utils/BasicTest.java
@@ -2,13 +2,11 @@ package com.utils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import java.util.concurrent.TimeUnit;
 
 
 public abstract class BasicTest {
--- src/test/java/com/Bai_17.java
+++ src/test/java/com/Bai_17.java
@@ -1,15 +1,13 @@
 package com;
 
-import org.bouncycastle.jcajce.provider.asymmetric.dsa.DSASigner.dsa224;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.utils.BasicTest;
 
-public class Bai_16 extends BasicTest {
+public class Bai_17 extends BasicTest {
 
 
     @Test()
