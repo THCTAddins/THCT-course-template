d336ebe4230dcbce286e4db2e49a94915e3579fe
lulmaoatwork <lulmaoatwork@gmail.com>
2023-12-27 17:48:12 +0700

    delete import

--- src/test/java/com/Bai_18.java
+++ src/test/java/com/Bai_18.java
@@ -2,8 +2,6 @@ package com;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.safari.SafariDriver.WindowType;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
