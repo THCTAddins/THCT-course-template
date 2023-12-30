a1a37a8cde191a6256e08b6aae700f40f1c9c6ef
THCTAddins <tohemcoten@gmail.com>
2023-12-30 13:06:47 +0700

    add go to homepage

--- src/test/java/com/Bai_17.java
+++ src/test/java/com/Bai_17.java
@@ -24,6 +24,10 @@ public class Bai_17 extends BasicTest {
         driver.findElement(By.id("password")).sendKeys("fto24137@omeie.com");
         driver.findElement(By.xpath("//button[@name='login']")).click();
 
+        //Back to homepage
+        driver.findElement(By.xpath("//a[contains(text(),'Trang chủ')]")).click();
+        driver.findElement(By.xpath("//a/strong")).click();
+
         // 'Tài khoản' hyperlink click
         driver.findElement(By.xpath("//a[contains(text(),'Tài khoản')]")).click();
         Assert.assertEquals(driver.getCurrentUrl(), "https://bantheme.xyz/hathanhauto/tai-khoan/edit-account/");
