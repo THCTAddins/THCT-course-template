991810e5e2030c2b0f8192102ca9d2a11bfb0375
lulmaoatwork <lulmaoatwork@gmail.com>
2024-01-10 11:10:35 +0700

    :D

--- src/test/java/com/Bai_21.java
+++ src/test/java/com/Bai_21.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import com.utils.BasicTest;
