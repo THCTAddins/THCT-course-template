b156ad62891fb00125d211cc174719c07c96300b
lulmaoatwork <lulmaoatwork@gmail.com>
2024-01-25 14:40:23 +0700

    delete import

--- src/test/java/com/Bai_23.java
+++ src/test/java/com/Bai_23.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
-import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import com.pages.LoginPage;
